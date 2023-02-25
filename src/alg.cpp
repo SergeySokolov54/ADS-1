c230339b6d9a61f33007099cc25a94b51688f4c7
SergeySokolov54 <116427633+SergeySokolov54@users.noreply.github.com>
2023-02-26 00:53:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,15 +7,15 @@ bool checkPrime(uint64_t value) {
     if (value == 0) { return false; }
     int k = 0;
     for (int i = 2; i < value; i++) {
-        if (value % i != 0) { 
-            continue; 
+        if (value % i != 0) {
+            continue;
         }
-        else { 
-            k = 1; 
+        else {
+            k = 1;
         }
     }
     if (k == 1) { return false; }
-    else{ return true; }
+    else { return true; }
 }
 
 uint64_t nPrime(uint64_t n) {
@@ -46,11 +46,13 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
     int k = 0;
-    for (int i = 2;i < hbound; i++) {
+    for (int i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
             k += i;
         }
-        else { continue; }
+        else {
+            continue;
+        }
     }
     return k;
 }
